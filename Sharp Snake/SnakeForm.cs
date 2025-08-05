3a1642d8b4cfb899434c683da937ecf51dbeead5
Laitheis <gosha.reshetnikov2000@gmail.com>
2025-08-05 23:45:00 +0300

    Rewrite complete

--- Sharp Snake/SnakeForm.cs
+++ Sharp Snake/SnakeForm.cs
@@ -1,257 +1,3 @@
-//using FormsTimer = System.Windows.Forms.Timer;
-
-//namespace Sharp_Snake
-//{
-//    public partial class SnakeForm : Form
-//    {
-//        private void SnakeForm_Load(object sender, EventArgs e)
-//        {
-//            this.Text = "Змейка";
-//            this.DoubleBuffered = true;
-//            this.KeyPreview = true;
-//            this.FormBorderStyle = FormBorderStyle.FixedSingle;
-//            this.MaximizeBox = false;
-//        }
-
-//        private const int CellSize = 40;
-//        private const int WidthInCells = 3;
-//        private const int HeightInCells = 3;
-
-//        private FormsTimer gameTimer;
-//        private Snake snake;
-//        private Point food;
-//        private int score;
-//        private bool isGameRunning;
-//        private Random random;
-//        private Direction currentDirection;
-//        private Direction nextDirection;
-
-//        public SnakeForm()
-//        {
-//            gameTimer = new FormsTimer();
-//            snake = new Snake(new Point());
-//            random = new Random();
-
-//            InitializeComponent();
-//            InitializeGame();
-//        }
-
-//        private void InitializeGame()
-//        {
-//            gameTimer.Interval = 500;
-//            gameTimer.Tick += GameLoop;
-
-//            btnStart.Click += (s, e) => StartGame();
-//            btnRestart.Click += (s, e) => RestartGame();
-
-//            btnRestart.Enabled = false;
-//        }
-
-//        private void StartGame()
-//        {
-//            if (isGameRunning) return;
-
-//            snake = new Snake(new Point(WidthInCells / 2, HeightInCells / 2));
-//            currentDirection = Direction.Right;
-//            nextDirection = Direction.Right;
-//            score = 0;
-//            GenerateFood();
-
-//            isGameRunning = true;
-//            btnStart.Enabled = false;
-//            btnRestart.Enabled = false;
-
-//            gameTimer.Start();
-//            this.Focus();
-//        }
-
-//        private void RestartGame()
-//        {
-//            gameTimer.Stop();
-//            isGameRunning = false;
-//            btnStart.Enabled = true;
-//            btnRestart.Enabled = false;
-//            Invalidate();
-//        }
-
-//        private void GenerateFood()
-//        {
-//            do
-//            {
-//                food = new Point(
-//                    random.Next(0, WidthInCells),
-//                    random.Next(0, HeightInCells));
-//            }
-//            while (snake.Body.Contains(food));
-//        }
-
-//        private void GameLoop(object sender, EventArgs e)
-//        {
-//            currentDirection = nextDirection;
-//            snake.Move(currentDirection);
-
-//            // Проверка столкновений
-//            if (snake.Head.X < 0 || snake.Head.X >= WidthInCells ||
-//                snake.Head.Y < 0 || snake.Head.Y >= HeightInCells ||
-//                snake.Body.Skip(1).Any(segment => segment == snake.Head))
-//            {
-//                GameOver();
-//                return;
-//            }
-
-//            // Проверка съедания еды
-//            if (snake.Head == food)
-//            {
-//                snake.Grow();
-//                score++;
-//                GenerateFood();
-
-//                // Ускорение игры
-//                if (gameTimer.Interval > 50 && score % 5 == 0)
-//                {
-//                    gameTimer.Interval -= 5;
-//                }
-//            }
-
-//            Invalidate();
-//        }
-
-//        private void GameOver()
-//        {
-//            gameTimer.Stop();
-//            isGameRunning = false;
-//            btnRestart.Enabled = true;
-//            MessageBox.Show($"Игра окончена! Ваш счет: {score}", "Змейка");
-//        }
-
-//        protected override void OnPaint(PaintEventArgs e)
-//        {
-//            base.OnPaint(e);
-
-//            if (!isGameRunning) return;
-
-//            // Рисуем сетку
-//            for (int x = 0; x < WidthInCells; x++)
-//            {
-//                for (int y = 0; y < HeightInCells; y++)
-//                {
-//                    e.Graphics.DrawRectangle(
-//                        Pens.LightGray,
-//                        x * CellSize,
-//                        y * CellSize,
-//                        CellSize,
-//                        CellSize);
-//                }
-//            }
-
-//            // Рисуем змейку
-//            foreach (var segment in snake.Body)
-//            {
-//                e.Graphics.FillRectangle(
-//                    Brushes.Green,
-//                    segment.X * CellSize,
-//                    segment.Y * CellSize,
-//                    CellSize,
-//                    CellSize);
-//            }
-
-//            // Рисуем голову змейки другим цветом
-//            e.Graphics.FillRectangle(
-//                Brushes.DarkGreen,
-//                snake.Head.X * CellSize,
-//                snake.Head.Y * CellSize,
-//                CellSize,
-//                CellSize);
-
-//            // Рисуем еду
-//            e.Graphics.FillEllipse(
-//                Brushes.Red,
-//                food.X * CellSize,
-//                food.Y * CellSize,
-//                CellSize,
-//                CellSize);
-
-//            // Рисуем счет
-//            e.Graphics.DrawString(
-//                $"Счет: {score}",
-//                new Font("Arial", 12),
-//                Brushes.Black,
-//                10, 10);
-//        }
-
-//        protected override void OnKeyDown(KeyEventArgs e)
-//        {
-//            base.OnKeyDown(e);
-
-//            if (!isGameRunning) return;
-
-//            switch (e.KeyCode)
-//            {
-//                case Keys.Up when currentDirection != Direction.Down:
-//                    nextDirection = Direction.Up;
-//                    break;
-//                case Keys.Down when currentDirection != Direction.Up:
-//                    nextDirection = Direction.Down;
-//                    break;
-//                case Keys.Left when currentDirection != Direction.Right:
-//                    nextDirection = Direction.Left;
-//                    break;
-//                case Keys.Right when currentDirection != Direction.Left:
-//                    nextDirection = Direction.Right;
-//                    break;
-//            }
-//        }
-//    }
-
-//    public enum Direction
-//    {
-//        Up,
-//        Down,
-//        Left,
-//        Right
-//    }
-
-//    public class Snake
-//    {
-//        public List<Point> Body { get; }
-//        public Point Head => Body.First();
-
-//        public Snake(Point startPosition)
-//        {
-//            Body = new List<Point> { startPosition };
-//        }
-//        public void Move(Direction direction)
-//        {
-//            var newHead = Head;
-
-//switch (direction)
-//{
-//    case Direction.Up:
-//        newHead.Y--;
-//        break;
-//    case Direction.Down:
-//        newHead.Y++;
-//        break;
-//    case Direction.Left:
-//        newHead.X--;
-//        break;
-//    case Direction.Right:
-//        newHead.X++;
-//        break;
-//}
-
-//            Body.Insert(0, newHead);
-//            Body.RemoveAt(Body.Count - 1);
-//        }
-
-//        public void Grow()
-//        {
-//            Body.Add(Body.Last());
-//        }
-//    }
-//}
-
-
 using FormsTimer = System.Windows.Forms.Timer;
 
 namespace Sharp_Snake
@@ -271,8 +17,8 @@ namespace Sharp_Snake
         private Point _food;
 
         private FormsTimer _gameTimer;
-        private const int _fieldWidth = 5;
-        private const int _fieldHeight = 15;
+        private const int _fieldWidth = 4;
+        private const int _fieldHeight = 4;
         private const int _cellSize = 20;
 
         private bool _isGameStarted;
@@ -292,9 +38,9 @@ namespace Sharp_Snake
 
         private void InitializeGame()
         {
-            _snake = new Snake(new Point(_fieldWidth / 2, _fieldHeight / 2));
             _gameTimer = new FormsTimer();
             _random = new Random();
+            _snake = new Snake(new Point(_fieldWidth / 2, _fieldHeight / 2));
 
             btnRestart.Enabled = false;
 
@@ -307,6 +53,10 @@ namespace Sharp_Snake
         private void StartGame()
         {
             if (_isGameStarted) return;
+
+            _snake = new Snake(new Point(_fieldWidth / 2, _fieldHeight / 2));
+
+            _nextDirection = Direction.Up;
 
             btnStart.Enabled = false;
             btnRestart.Enabled = false;
@@ -323,6 +73,8 @@ namespace Sharp_Snake
 
         private void RestartGame()
         {
+            _isGameStarted = false;
+
             StartGame();
         }
 
@@ -348,9 +100,17 @@ namespace Sharp_Snake
             {
                 _snake.Grow();
 
-                if(_snake.Body.Count == _fieldHeight * _fieldWidth)
+                if (_snake.Body.Count - 1 == _fieldHeight * _fieldWidth)
                 {
+                    _snake.Body.Insert(0, _food);
+
+                    _isGameStarted = false;
+
+                    Invalidate();
+
                     Win();
+
+                    return;
                 }
 
                 GenerateFood();
@@ -363,6 +123,8 @@ namespace Sharp_Snake
         {
             _gameTimer.Stop();
 
+            btnRestart.Enabled = true;
+
             MessageBox.Show("Победа!");
         }
 
@@ -373,6 +135,8 @@ namespace Sharp_Snake
             _gameTimer.Stop();
 
             btnRestart.Enabled = true;
+
+            MessageBox.Show("Поражение.");
         }
 
         protected override void OnKeyDown(KeyEventArgs e)
@@ -412,6 +176,7 @@ namespace Sharp_Snake
         {
             base.OnPaint(e);
 
+
             for (int x = 0; x < _fieldWidth; x++)
             {
                 for (int y = 0; y < _fieldHeight; y++)
@@ -442,12 +207,15 @@ namespace Sharp_Snake
                 _cellSize,
                 _cellSize);
 
-            e.Graphics.FillEllipse(
-                Brushes.Red,
-                _food.X * _cellSize,
-                _food.Y * _cellSize,
-                _cellSize,
-                _cellSize);
+            if (_isGameStarted)
+            {
+                e.Graphics.FillEllipse(
+                    Brushes.Red,
+                    _food.X * _cellSize,
+                    _food.Y * _cellSize,
+                    _cellSize,
+                    _cellSize);
+            }
         }
 
         public enum Direction
